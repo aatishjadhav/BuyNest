ef5b51f871fc4cdaa04ca808ec5207c9d3faef57
Atish <atishjadhav835@gmail.com>
2025-03-17 15:45:35 +0530

    added products for Kids and Electronics category

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
   const navigate = useNavigate();
 
   const handleCategoryClick = (category) => {
-    navigate(`/products/categories/${category}`);  
+    navigate(`/products/categories/${category}`);
     dispatch(fetchByCategory(category));
   };
-  
+
   return (
     <>
       <div className="container">
@@ -26,43 +26,102 @@ const Home = () => {
         <div className="text-center py-5">
           <h2>Categories</h2>
           <div className="row py-3 rounded">
-            <div className="col-md-3 mb-3" style={{cursor: 'pointer'}}  onClick={() => handleCategoryClick("mens")}>
-              <div className="bg-light vspace=4" style={{ padding: "6rem 6rem" }}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-            <circle cx="8.5" cy="7" r="4"></circle>
-            <polyline points="17 11 19 13 23 9"></polyline>
-          </svg>
-                <h2 className="mt-3">Men</h2>
+            <div
+              className="col-md-3 mb-3"
+              style={{ cursor: "pointer" }}
+              onClick={() => handleCategoryClick("mens")}
+            >
+              <div
+                className="bg-light vspace=4"
+                style={{ padding: "6rem 6rem" }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="64"
+                  height="64"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                  <circle cx="8.5" cy="7" r="4"></circle>
+                  <polyline points="17 11 19 13 23 9"></polyline>
+                </svg>
+                <h4 className="mt-3">Men</h4>
               </div>
             </div>
-            <div className="col-md-3 mb-3" style={{cursor: 'pointer'}} onClick={() => handleCategoryClick("womens")}>
+            <div
+              className="col-md-3 mb-3"
+              style={{ cursor: "pointer" }}
+              onClick={() => handleCategoryClick("womens")}
+            >
               <div className="bg-light" style={{ padding: "6rem 6rem" }}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M16 2H8l-4 7h3l-4 7h4l3 5 .5-9h4l.5 9 3-5h4l-4-7h3l-4-7Z"></path>
-          </svg>
-                <h2>Women</h2>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="64"
+                  height="64"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M16 2H8l-4 7h3l-4 7h4l3 5 .5-9h4l.5 9 3-5h4l-4-7h3l-4-7Z"></path>
+                </svg>
+                <h4 className="mt-3">Women</h4>
               </div>
             </div>
-            <div className="col-md-3 mb-3" style={{cursor: 'pointer'}} onClick={() => handleCategoryClick("kids")}>
+            <div
+              className="col-md-3 mb-3"
+              style={{ cursor: "pointer" }}
+              onClick={() => handleCategoryClick("Kids")}
+            >
               <div className="bg-light" style={{ padding: "6rem 6rem" }}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M10 2v1h4V2"></path>
-            <path d="M13.8 8.8a2 2 0 0 0-3.6 0"></path>
-            <path d="M7 13.8a5 5 0 0 0 10 0"></path>
-            <path d="M17 15.2V16a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2v-.8"></path>
-            <path d="M12 22v-1"></path>
-          </svg>
-                <h2>Kids</h2>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="64"
+                  height="64"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M10 2v1h4V2"></path>
+                  <path d="M13.8 8.8a2 2 0 0 0-3.6 0"></path>
+                  <path d="M7 13.8a5 5 0 0 0 10 0"></path>
+                  <path d="M17 15.2V16a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2v-.8"></path>
+                  <path d="M12 22v-1"></path>
+                </svg>
+                <h4 className="mt-3">Kids</h4>
               </div>
             </div>
-            <div className="col-md-3 mb-3" style={{cursor: 'pointer'}} onClick={() => handleCategoryClick("electronics")}>
+            <div
+              className="col-md-3 mb-3"
+              style={{ cursor: "pointer" }}
+              onClick={() => handleCategoryClick("Electronics")}
+            >
               <div className="bg-light" style={{ padding: "6rem 6rem" }}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <rect x="5" y="2" width="14" height="20" rx="2" ry="2"></rect>
-            <path d="M12 18h.01"></path>
-          </svg>
-                <h2>Electronics</h2>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="64"
+                  height="64"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <rect x="5" y="2" width="14" height="20" rx="2" ry="2"></rect>
+                  <path d="M12 18h.01"></path>
+                </svg>
+                <h4 className="mt-3">Electronics</h4>
               </div>
             </div>
           </div>
