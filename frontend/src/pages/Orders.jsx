47d14fe23089759cf521f3b89cec29617f6ef46a
Atish <atishjadhav835@gmail.com>
2025-04-20 00:04:24 +0530

    added loader in orders page

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -5,7 +5,7 @@ import { fetchOrders } from "../slices/orderSlice";
 
 const Orders = () => {
   const dispatch = useDispatch();
-  const { orders } = useSelector((state) => state.orders);
+  const { orders, status, error } = useSelector((state) => state.orders);
   console.log("orders", orders);
 
   useEffect(() => {
@@ -18,33 +18,46 @@ const Orders = () => {
 
       <div className="container py-3">
         <h3 className="text-center">My Orders</h3>
-        {orders?.length > 0 ? (
-          <div>
-            {orders?.map((order, index) => (
-              <div className="card my-3" key={index}>
-                <div className="card-body">
-                  <h5 className="card-title">Order #{order._id}</h5>
-                  <p>
-                    <b>Total Amount:</b> ₹{order.total}
-                  </p>
-
-                  <h6>Items:</h6>
-                  <ul>
-                    {order?.items?.map((item, idx) => (
-                      <li key={idx}>
-                        <p>
-                          {item.productId.name} - Quantity: {item.quantity}
-                        </p>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            ))}
+        {status === "loading" ? (
+          <div
+            className="d-flex justify-content-center align-items-center"
+            style={{ height: "70vh" }}
+          >
+            <div className="spinner-border text-primary" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
           </div>
         ) : (
-          <div className="text-center py-5">
-            <p>No orders found.</p>
+          <div>
+            {orders?.length > 0 ? (
+              <div>
+                {orders?.map((order, index) => (
+                  <div className="card my-3" key={index}>
+                    <div className="card-body">
+                      <h5 className="card-title">Order #{order._id}</h5>
+                      <p>
+                        <b>Total Amount:</b> ₹{order.total}
+                      </p>
+
+                      <h6>Items:</h6>
+                      <ul>
+                        {order?.items?.map((item, idx) => (
+                          <li key={idx}>
+                            <p>
+                              {item.productId.name} - Quantity: {item.quantity}
+                            </p>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div className="text-center py-5">
+                <p>No orders found.</p>
+              </div>
+            )}
           </div>
         )}
       </div>
